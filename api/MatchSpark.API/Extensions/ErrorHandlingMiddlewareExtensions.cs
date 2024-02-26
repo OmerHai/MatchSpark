8602e8ecc1517b0d7ad19b80148d1ae83c49c52c
Omer Haimovich <omer16493@gmail.com>
2024-02-26 17:27:15 +0200

    Add ErrorHandlingMiddleware for consistently and orderly manage errors in the application
    - Add Middlewares folder which contains all middlewares.
    - Add ErrorHandlingMiddlware.cs that responsible for error management.
    - ErrorHandlingMiddlware.cs includes a function named HandleExceptionAsync that returns to the client an error with code 500 and an explanation of the error in json format.
    - Add Extensions folder which contains all extensions methods.
    - Add ErrorHandlingMiddlewareExtensions.cs that responsible for adding ErrorHandlingMiddlware to app.
    - ErrorHandlingMiddlewareExtensions.cs includes a fuction named UseErrorHandlingMiddleware that returns the app with the ErrorHandlingMiddleware.

---
+++ api/MatchSpark.API/Extensions/ErrorHandlingMiddlewareExtensions.cs
@@ -0,0 +1,13 @@
+
+using MatchSpark.API.Middlewares;
+
+namespace MatchSpark.API.Extensions
+{
+    public static class ErrorHandlingMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseErrorHandlingMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ErrorHandlingMiddleware>();
+        }
+    }
+}
