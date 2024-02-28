0a90620ae9fc445d0f7363ce59cb17b24901b37b
Omer Haimovich <omer16493@gmail.com>
2024-02-28 16:08:06 +0200

    Configure ApplicationDbContext and Initial Migration
    -Implemented ApplicationDbContext with DbSet for AppUser, enabling user table creation in the database.
    - Updated Program.cs to configure ApplicationDbContext.
    - Removed the unused Class1.cs file.
    - Added initial EF Core migration for database schema setup.
    - Configured the default connection string in appsettings.json for MatchSparkDb connection.

---
+++ api/MatchSpark.Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using MatchSpark.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MatchSpark.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<AppUser> Users { get; set; }
+}
