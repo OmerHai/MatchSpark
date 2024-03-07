75c4ff649e7cde58fe1aee34ee0081270aa4c62d
Omer Haimovich <omer16493@gmail.com>
2024-03-07 14:00:29 +0200

    Add checking whether the email already exists when registering a single user

--- api/MatchSpark.API/Controllers/UsersController.cs
+++ api/MatchSpark.API/Controllers/UsersController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using MatchSpark.Data.Services;
 using MatchSpark.API.DTOs;
 using MatchSpark.Core.Entities;
+using MatchSpark.Core.Common;
 
 namespace MatchSpark.API.Controllers
 {
@@ -19,10 +20,10 @@ namespace MatchSpark.API.Controllers
         public async Task<IActionResult> Register([FromBody] UserRegistrationDto newUserDto)
         {
             AppUser newUser = new AppUser { Email = newUserDto.Email };
-            bool result = await _userServices.RegisterUserAsync(newUser, newUserDto.Password);
-            if (result)
+            OperationResult result = await _userServices.RegisterUserAsync(newUser, newUserDto.Password);
+            if (result.Success)
                 return Ok("User registered successfully.");
-            return BadRequest("User registration failed.");
+            return BadRequest(result.ErrorMessage);
         }
     }
 }
