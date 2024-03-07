fbe0fa22a027715b38db634c9996b8a0973b486c
Omer Haimovich <omer16493@gmail.com>
2024-03-07 14:54:43 +0200

    Add .csproj file to the repository

--- api/MatchSpark.API/Program.cs
+++ api/MatchSpark.API/Program.cs
@@ -2,9 +2,23 @@ using MatchSpark.API.Extensions;
 using MatchSpark.Data.Services;
 using MatchSpark.Data;
 using Microsoft.EntityFrameworkCore;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
+var jwtSettings = builder.Configuration.GetSection("JwtSettings");
+var secretKey = Encoding.UTF8.GetBytes(jwtSettings["Secret"]);
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options => {
+        options.TokenValidationParameters = new TokenValidationParameters {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(secretKey),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -12,6 +26,8 @@ builder.Services.AddScoped<UserServices>();
 
 var app = builder.Build();
 
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseErrorHandlingMiddleware();
 app.UseHttpsRedirection();
 app.MapControllers();
